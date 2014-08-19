f8ca212bf220cd63cd31e89cdf7d9abf45dc8420
Phil Hagelberg <technomancy@gmail.com>
2014-08-19 15:55:56 -0700

    init

---
+++ src/clojure_getting_started/web.clj
@@ -0,0 +1,26 @@
+(ns clojure-getting-started.web
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
+            [compojure.route :as route]
+            [clojure.java.io :as io]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
+
+(defn splash []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (pr-str ["Hello" :from 'Heroku])})
+
+(defroutes app
+  (GET "/" []
+       (splash))
+  (ANY "*" []
+       (route/not-found (slurp (io/resource "404.html")))))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
+
+;; For interactive development:
+;; (.stop server)
+;; (def server (-main))

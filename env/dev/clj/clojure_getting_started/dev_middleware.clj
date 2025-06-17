b2bf320143848ea2b19a27152f00b16a4b67145a
Woracgret <woachet143@gmail.com>
2025-06-17 11:43:43 +0700

    Update dev_middleware.clj
    M
    
    Signed-off-by: Woracgret <woachet143@gmail.com>

--- env/dev/clj/clojure_getting_started/dev_middleware.clj
+++ env/dev/clj/clojure_getting_started/dev_middleware.clj
@@ -1,9 +1,9 @@
 (ns clojure-getting-started.dev-middleware
   (:require
-   [clojure-getting-started.config :refer [env]]
-   [ring.middleware.reload :refer [wrap-reload]]
-   [selmer.middleware :refer [wrap-error-page]]
-   [prone.middleware :refer [wrap-exceptions]]))
+   [clojure-getting-started.config :refer [env]
+   [ring.middleware.reload :refer [wrap-reload]
+   [selmer.middleware :refer [wrap-error-page]
+   [prone.middleware :refer [wrap-exceptions]
 
 (defn wrap-dev [handler]
   (-> handler
@@ -11,3 +11,4 @@
       wrap-error-page
       ;; disable prone middleware, it can not handle async
       (cond-> (not (env :async?)) (wrap-exceptions {:app-namespaces ['clojure-getting-started]}))))
+"ยินดีต้อนรับสู่คลิปบอร์ดของ Gboard ข้อความที่คุณคัดลอกจะบันทึกไว้ที่นี่

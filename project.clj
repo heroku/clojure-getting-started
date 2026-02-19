6d52ae79da0f86da08a5ad350b4b9501c11c6b07
Manuel Fuchs <manuel.fuchs@salesforce.com>
2026-02-19 13:12:24 +0100

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject clojure-getting-started "0.1.0"
-  :dependencies [[org.clojure/clojure "1.12.3"]
-                 [ring/ring-core "1.9.6"]
-                 [ring/ring-jetty-adapter "1.9.6"]
-                 [ring/ring-defaults "0.3.4"]
-                 [compojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.12.4"]
+                 [ring/ring-core "1.15.3"]
+                 [ring/ring-jetty-adapter "1.15.3"]
+                 [ring/ring-defaults "0.7.0"]
+                 [compojure "1.7.2"]
                  [org.clojure/java.jdbc "0.7.12"]
-                 [org.postgresql/postgresql "42.7.2"]]
+                 [org.postgresql/postgresql "42.7.10"]]
   :main com.heroku.clojure-getting-started
   :aot [com.heroku.clojure-getting-started]
   :uberjar-name "clojure-getting-started.jar"
